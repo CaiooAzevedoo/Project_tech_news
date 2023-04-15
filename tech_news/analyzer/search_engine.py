39d50499ac848b802e3528fefe28093d289f200a
Caio <caiusito@gmail.com>
2023-04-15 11:16:16 -0300

    Req 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+from tech_news.database import find_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = find_news()
+    result = list()
+    for item in news:
+        if (title.upper() in item['title'].upper()):
+            result.append((item['title'], item['url']))
+    return result
 
 
 # Requisito 8
