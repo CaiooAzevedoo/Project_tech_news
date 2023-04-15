27d4df63b83ddb8615cf26d1a4c4ea1649aaee41
Caio <caiusito@gmail.com>
2023-04-15 11:58:18 -0300

    Req 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+import datetime
 
 
 # Requisito 7
@@ -13,7 +14,24 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    news = find_news()
+    result = list()
+
+    try:
+        datetime.date.fromisoformat(date)
+        # https://docs.python.org/3/library/datetime.html
+    except ValueError:
+        raise ValueError('Data inválida')
+
+    day = date[8:10]
+    month = date[5:7]
+    year = date[0:4]
+    formatted_date = f"{day}/{month}/{year}"
+
+    for item in news:
+        if (formatted_date == item['timestamp']):
+            result.append((item['title'], item['url']))
+    return result
 
 
 # Requisito 9
